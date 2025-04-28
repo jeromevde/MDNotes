20c4b99633cec07f03206d25e62da012a052fe93
jerome <je.vanderelst@gmail.com>
2025-04-28 16:18:49 +0200

    github workflow

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/MDNotes/' : '/',
   devServer: {
     host: '127.0.0.1',
     port: 8080,
-    allowedHosts: 'all'
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: 'ws://127.0.0.1:8080/ws'
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
   }
 })
